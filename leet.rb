ab0602dcbf5952e3013d0620ee982e0bfb0443f2
kano1201 <kano.nakamura3@gmail.com>
2021-06-24 06:08:09 +0000

    practice1

--- leet.rb
+++ leet.rb
@@ -1,1 +1,25 @@
-1
+input_line = gets
+strings = input_line.to_s.split('')
+
+convert = []
+strings.each do |chara|
+    #ループ内で行う処理
+    case chara
+    when "A" then
+     convert.push("4") #処理
+    when "E" then
+     convert.push("3")
+    when "G" then
+     convert.push("6")
+    when "I" then
+     convert.push("1")
+    when "O" then
+     convert.push("0")
+        
+    else #どれにも当てはまらない時
+     convert.push(chara)
+    end
+end
+
+output = convert.join
+puts output
